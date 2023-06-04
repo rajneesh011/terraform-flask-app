5d343ea6773c40f669158753691900a1d18a7449
Rajneesh Singh <63272969+rajneesh011@users.noreply.github.com>
2023-06-04 20:42:31 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 provider "aws" {
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
+  access_key = ${{ secrets.AWS_ACCESS_KEY_ID }}
+  secret_key = ${{ secrets.AWS_SECRET_ACCESS_KEY }}
   region     = "us-east-1"
 }
 
